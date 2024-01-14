419f028c7204aeea66a70fca5736cc5e8ac08d43
Muris Ceman <murisceman@gmail.com>
2024-01-14 21:36:50 +0100

    v3.9.3

--- src/cli/init.ts
+++ src/cli/init.ts
@@ -108,7 +108,7 @@ export default defineCommand({
 
       // Add pruvious
       packageJson.dependencies ??= {}
-      packageJson.dependencies.pruvious = '^3.9.2'
+      packageJson.dependencies.pruvious = '^3.9.3'
 
       fs.writeJsonSync(join(template.dir, 'package.json'), packageJson, { spaces: 2 })
     } catch (err) {
@@ -213,7 +213,7 @@ export function replaceReadme(cwd?: string) {
     path.resolve(cwd ?? process.cwd(), 'README.md'),
     [
       '# Project',
-      'This project was generated with [Pruvious](https://pruvious.com) CLI version 3.9.2.',
+      'This project was generated with [Pruvious](https://pruvious.com) CLI version 3.9.3.',
       '',
       'Documentation: [https://pruvious.com/docs](https://pruvious.com/docs)',
       '',
