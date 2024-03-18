38ca163e3a7b718a3bde03e5c829bc614c4cfaa5
Muris Ceman <murisceman@gmail.com>
2024-03-18 23:32:12 +0100

    v3.11.0

--- src/cli/init.ts
+++ src/cli/init.ts
@@ -107,7 +107,7 @@ export default defineCommand({
       packageJson.dependencies['vue-router'] = '^4.3.0'
 
       // Add pruvious
-      packageJson.dependencies.pruvious = '^3.10.8'
+      packageJson.dependencies.pruvious = '^3.11.0'
 
       // Sort
       packageJson.dependencies = Object.fromEntries(
@@ -217,7 +217,7 @@ export function replaceReadme(cwd?: string) {
     path.resolve(cwd ?? process.cwd(), 'README.md'),
     [
       '# Project',
-      'This project was generated with [Pruvious](https://pruvious.com) CLI version 3.10.8.',
+      'This project was generated with [Pruvious](https://pruvious.com) CLI version 3.11.0.',
       '',
       'Documentation: [https://pruvious.com/docs](https://pruvious.com/docs)',
       '',
