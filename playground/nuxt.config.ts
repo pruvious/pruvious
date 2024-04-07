6873020670dc7bc912481abeaafa264cafa969dc
Muris Ceman <murisceman@gmail.com>
2024-04-07 23:17:04 +0200

    chore: reset `nuxt.config.ts` in playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -8,14 +8,6 @@ export default defineNuxtConfig({
   },
   modules: ['pruvious'],
   pruvious: {
-    pageCache: { type: 'redis' },
-    dashboard: {
-      baseComponents: {
-        misc: {
-          Logo: './components/dashboard/misc/Logo.vue',
-        },
-      },
-    },
     database: 'sqlite:./dev.db',
     jwt: {
       secretKey: 'xxx',
