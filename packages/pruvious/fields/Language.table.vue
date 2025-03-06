1dde2634260a72a25de07a34ada180abb270b9bd
Muris Ceman <murisceman@gmail.com>
2025-03-06 17:41:25 +0100

    feat: add table component for `language` field

---
+++ packages/pruvious/fields/Language.table.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-if="language" class="pui-row">
+      <PUIBadge color="secondary" class="pui-uppercase">{{ language.code }}</PUIBadge>
+      <span class="pui-truncate">{{ language.name }}</span>
+    </div>
+    <PruviousTableFieldFallback v-else :modelValue="modelValue" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PruviousTableFieldFallback } from '#components'
+import { languages } from '#pruvious/client'
+
+const props = defineProps({
+  /**
+   * The casted field value.
+   */
+  modelValue: {
+    type: null as unknown as PropType<any>,
+  },
+})
+
+const language = languages.find(({ code }) => code === props.modelValue)
+</script>
