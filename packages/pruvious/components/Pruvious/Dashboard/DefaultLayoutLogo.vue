7bbb5714128529a426d29ed46561cf9ba5d4f65a
Muris Ceman <murisceman@gmail.com>
2025-04-24 23:38:10 +0200

    feat: link dashboard logo

--- packages/pruvious/components/Pruvious/Dashboard/DefaultLayoutLogo.vue
+++ packages/pruvious/components/Pruvious/Dashboard/DefaultLayoutLogo.vue
@@ -1,9 +1,19 @@
 <template>
-  <PruviousDashboardLogoMark class="p-logo" />
+  <NuxtLink :title="__('pruvious-dashboard', 'Go back home')" :to="link" class="p-logo pui-raw">
+    <PruviousDashboardLogoMark />
+  </NuxtLink>
 </template>
+
+<script lang="ts" setup>
+import { __, dashboardBasePath } from '#pruvious/client'
+import { withoutTrailingSlash } from '@pruvious/utils'
+
+const link = withoutTrailingSlash(dashboardBasePath)
+</script>
 
 <style scoped>
 .p-logo {
+  display: block;
   width: auto;
   height: 1.75rem;
   color: hsl(var(--pui-card));
@@ -12,5 +22,11 @@
 
 .dark .p-logo {
   filter: brightness(1.5);
+}
+
+.p-logo :deep(svg) {
+  display: block;
+  width: auto;
+  height: 100%;
 }
 </style>
