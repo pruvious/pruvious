09e63269c82c28ee3e8cdea088c4415f56c42b23
Muris Ceman <murisceman@gmail.com>
2026-01-24 16:19:05 +0100

    chore: cache `hub-app` builds

--- packages/hub-app/scripts/build.mjs
+++ packages/hub-app/scripts/build.mjs
@@ -1,9 +1,36 @@
+import { hashSources } from '@pruvious/cli-utils'
+import { execa } from 'execa'
 import fs from 'node:fs'
 import { fileURLToPath } from 'node:url'
 import { dirname, join, resolve } from 'pathe'
 
 const url = new URL(import.meta.url)
 const rootDir = resolve(dirname(fileURLToPath(url)), '..')
+const outputDir = join(rootDir, '.output')
+const sources = [
+  'app',
+  'public',
+  'scripts',
+  'server',
+  'LICENSE',
+  'nuxt.config.ts',
+  'package.json',
+  'pnpm-workspace.yaml',
+  'README.md',
+  'tsconfig.json',
+].map((file) => join(rootDir, file))
+const hash = hashSources(sources)
+const currentHashFile = join(rootDir, '.build')
+
+if (
+  !fs.existsSync(join(outputDir, 'server', 'index.mjs')) ||
+  !fs.existsSync(currentHashFile) ||
+  fs.readFileSync(currentHashFile, 'utf-8') !== hash
+) {
+  await execa('pnpm', ['exec', 'nuxt', 'build'], { cwd: rootDir, stdio: 'inherit' })
+  fs.writeFileSync(currentHashFile, hash, 'utf-8')
+}
+
 const distDir = join(rootDir, 'dist')
 
 if (fs.existsSync(distDir)) {
