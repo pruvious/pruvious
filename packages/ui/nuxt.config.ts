6d367b06c07eb44cf0158f859d81c9d92d36c46b
Muris Ceman <murisceman@gmail.com>
2026-04-05 18:39:42 +0200

    chore: update dependencies

--- packages/ui/nuxt.config.ts
+++ packages/ui/nuxt.config.ts
@@ -2,4 +2,9 @@ export default defineNuxtConfig({
   colorMode: { classSuffix: '' },
   compatibilityDate: '2025-07-15',
   modules: ['@nuxt/icon', '@nuxtjs/color-mode'],
+  vite: {
+    optimizeDeps: {
+      include: ['@vueuse/core', '@vueuse/integrations/useFocusTrap'],
+    },
+  },
 })
